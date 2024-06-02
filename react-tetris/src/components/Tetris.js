bef770b5338d560e0454d8fcec2e823ca0f32416
RafaelRatti <rafarattibarbosa@gmail.com>
2024-06-02 19:55:11 -0300

    atualizaçãod e projeto

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,25 +1,29 @@
 import React from "react";
 
 import { createStage} from "../gameHelpers";
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 // Components
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { StyledCell } from "./styles/StyledCell";
 
 const Tetris = () => {
     return(
-        <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
-                </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text="Score" />
+                        <Display text="Rows" />
+                        <Display text="Level" />
+                    </div>
                 <StartButton />
-            </aside>
-        </div>
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 };
 
